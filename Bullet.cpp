88b0465ad84c906072ccc3bd778b41cf2026ac23
zlatan-cic <smajicz999@gmail.com>
2025-07-28 14:57:14 +0200

    Implement Bullet class and shooting functionality

---
+++ Bullet.cpp
@@ -0,0 +1,115 @@
+﻿#include "Bullet.h"
+
+// The Constructor
+Bullet::Bullet()
+{
+	// Making a size of bullet 2x2
+	// Pravimo metak veličine 2x2 piksela
+	m_BulletShape.setSize(sf::Vector2f(5, 5));
+}
+
+// start X and Y are position of a player
+// target X and Y are position of a target
+// startX i startY su pozicija igrača, targetX i targetY su pozicija mete
+void Bullet::shoot(float startX, float startY, float targetX, float targetY)
+{
+	m_InFlight = true; // Bullet is now flying !!!
+	m_Position.x = startX;
+	m_Position.y = startY;
+
+	// Calculate the gradient of the flight path
+	// Izračunavanje nagiba putanje (vektorski pravac)
+	// Trigometrija !!!
+	float gradient = (startX - targetX) / (startY - targetY);
+
+	// Any gradient less than 0 needs to be positive
+	// Ako je nagib negativan, pretvaramo ga u pozitivan (radimo s pravcem kasnije)
+	if (gradient < 0)
+	{
+		gradient *= -1;
+	}
+
+	// Calculate the ratio between x and y
+	// Računamo koliko ide po Y a koliko po X da ukupna brzina bude ista
+	float ratioXY = m_BulletSpeeds / (1 + gradient);
+
+	// Set the "speed" horizontally and vertically
+	// Postavljamo brzinu po X i Y osama
+	m_BulletDistanceY = ratioXY;
+	m_BulletDistanceX = ratioXY * gradient;
+
+	// Point the bullet in the right direction
+	// Ako meta levo/gore, menjamo smer brzine
+	if (targetX < startX)
+	{
+		m_BulletDistanceX *= -1;
+	}
+	if (targetY < startY)
+	{
+		m_BulletDistanceY *= -1;
+	}
+
+	// Set a max range of 1000 pixels ( Domet metka )
+	// Bullet will disappear after traveling 1000px in any direction
+	float range = 1000;
+	m_MinX = startX - range;
+	m_MaxX = startX + range;
+	m_MinY = startY - range;
+	m_MaxY = startY + range;
+
+	// Position the bullet ready to be drawn
+	// Postavljamo grafički oblik metka na trenutnu poziciju
+	m_BulletShape.setPosition(m_Position);
+}
+
+// Stop the bullet
+// Zaustavi metak – više nije u letu
+void Bullet::stop()
+{
+	m_InFlight = false;
+}
+
+// Return if bullet is flying
+// Da li je metak trenutno u letu (true ili false)
+bool Bullet::isInFlight()
+{
+	return m_InFlight;
+}
+
+// Get the position (bounding box) of the bullet
+// Vraća poziciju metka kao pravougaonik – koristi se za koliziju
+FloatRect Bullet::getPosition()
+{
+	return m_BulletShape.getGlobalBounds();
+}
+
+// Get the shape of the bullet for drawing
+// Vraća oblik metka (pravougaonik) da se nacrta na ekranu
+RectangleShape Bullet::getShape()
+{
+	return m_BulletShape;
+}
+
+void Bullet::update(float elapsedTime)
+{
+	// Add velocity to position — real-time motion
+	// Dodaj brzinu na poziciju — kretanje kroz vreme
+	m_Position.x += m_BulletDistanceX * elapsedTime;
+	m_Position.y += m_BulletDistanceY * elapsedTime;
+
+	// Move the bullet
+	// Pomera metak !!!!!!!!
+	m_BulletShape.setPosition(m_Position);
+
+	// Has the bullet gone out of range?
+	// da li je metak izaso iz "dozvoljene zone"
+	if (m_Position.x < m_MinX ||
+		m_Position.x > m_MaxX ||
+		m_Position.y < m_MinY ||
+		m_Position.y > m_MaxY
+		)
+	{
+		m_InFlight = false;
+	}
+}
+
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "Player.h"
 #include "ZombieArena.h"
 #include "TextureHolder.h" //
+#include "Bullet.h"
 
 using namespace sf;
 
@@ -68,16 +69,41 @@ int main()
     Texture textureBackground = TextureHolder::GetTexture("graphics/background_sheet.png");
 
 
-    /////
+    ///// NAuci dobro /////
     // Prepare for a horde of zombies
     int numZombies;
     int numZombiesAlive;
     Zombie* zombies = nullptr;
+    // 100 bullets 
+    Bullet bullets[100];
+    int currentBullet  = 0;
+    int bulletsSpare = 24;
+    int bulletsInClip = 6;
+    int clipSide = 6;
+    float fireRate = 2;
+
+    //Where was the fire button pressed?
+    Time lastPressed;
+    
+    // Hide the mouse pointer and replace it with crosshair
+    window.setMouseCursorVisible(false);
+    Sprite spriteCrosshair;
+    Texture textureCrosshair = TextureHolder::GetTexture("graphics/crosshair.png");
+    spriteCrosshair.setTexture(textureCrosshair);
+    spriteCrosshair.setOrigin(25, 25);
+
     ////
 
     // The main game loop // /// // /// ///
     while (window.isOpen())
     {
+        Time dt = clock.restart();
+
+        // Update the total game time
+        gameTimeTotal += dt;
+
+        float dtAsSeconds = dt.asSeconds();
+
         /*
         ************
         Handle input
@@ -109,7 +135,26 @@ int main()
                 }
                 if (state == State::PLAYING)
                 {
-
+                    // Reloading
+                    if (event.key.code == Keyboard::R)
+                    {
+                        if (bulletsSpare >= clipSide)
+                        {
+                            // Plenty of bullets. Realod.
+                            bulletsInClip = clipSide;
+                            bulletsSpare -= clipSide;
+                        }
+                        else if (bulletsSpare > 0)
+                        {
+                            // Only few bullets left
+                            bulletsInClip = bulletsSpare;
+                            bulletsSpare = 0;
+                        }
+                        else
+                        {
+                            ///
+                        }
+                    }
                 }
 
             }
@@ -159,6 +204,26 @@ int main()
             {
                 player.stopRight();
             }
+            if (Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                if (gameTimeTotal.asMilliseconds() - lastPressed.asMilliseconds() > 1000 / fireRate && bulletsInClip > 0)
+                {
+                    // Pass the centre of the player
+                    // and the centre of the cross-hair
+                    // to the shoot function
+                    bullets[currentBullet].shoot(player.getCenter().x, player.getCenter().y, mouseWorldPosition.x, mouseWorldPosition.y);
+                    currentBullet ++;
+                    if (currentBullet  > 99)
+                    {
+                        currentBullet  = 0;
+                        std::cout << "Fired bullet #" << currentBullet << std::endl;
+                    }
+                    lastPressed = gameTimeTotal;
+                    bulletsInClip--;
+                }
+                std::cout << "Mouse clicked!" << std::endl;
+            }// End fire a bullet*******
+
         }// End WASD while playing
 
         // Handle the LEVELING up state
@@ -232,14 +297,19 @@ int main()
         if (state == State::PLAYING)
         {
             
+            //// GORE SAM PREBACIO JER ONDA RADI /////
             // Update the delta time
-            Time dt = clock.restart();
+            //Time dt = clock.restart();
 
-            // Update the total game time
-            gameTimeTotal += dt;
+            //// Update the total game time
+            //gameTimeTotal += dt;
 
-            // Make a fraction of 1 from the delta time
-            float dtAsSeconds = dt.asSeconds();
+            //float dtAsSeconds = dt.asSeconds();
+
+            ////////
+
+            //// Make a decimal fraction of 1 from the delta time
+            //float dtAsSeconds = dt.asSeconds();
 
             // Where is the mouse pointer
             mouseScreenPosition = Mouse::getPosition();
@@ -248,6 +318,9 @@ int main()
             // based coordinates of mainView
             mouseWorldPosition = window.mapPixelToCoords(
                 Mouse::getPosition(), mainView);
+
+            // Set the crosshair to the mouse world location
+            spriteCrosshair.setPosition(mouseWorldPosition);
 
             // Update the player
             player.update(dtAsSeconds, Mouse::getPosition());
@@ -267,6 +340,14 @@ int main()
                 if (zombies[i].isAlive())
                 {
                     zombies[i].update(dt.asSeconds(), playerPosition);
+                }
+            }
+            // Update any bullets that are in-flight
+            for (int i = 0; i < 100; i++)
+            {
+                if (bullets[i].isInFlight())
+                {
+                    bullets[i].update(dtAsSeconds);
                 }
             }
 
@@ -297,22 +378,32 @@ int main()
             {
                 window.draw(zombies[i].getSprite());
             }
+            for (int i = 0; i < 100; i++)
+            {
+                if (bullets[i].isInFlight())
+                {
+                    window.draw(bullets[i].getShape());
+                }
+            }
             ////****////
 
             // Draw the player
             window.draw(player.getSprite());
+
+            //Draw the crosshair
+            window.draw(spriteCrosshair);
         }
         if (state == State::LEVELING_UP)
         {
-            std::cout << "test from Stete::LEVELING_UP";
+            //std::cout << "test from Stete::LEVELING_UP";
         }
         if (state == State::PAUSED)
         {
-            std::cout << "test from Stete::PAUSED";
+            //std::cout << "test from Stete::PAUSED";
         }
         if (state == State::GAME_OVER)
         {
-            std::cout << "test from Stete::LEVELING_UP";
+            //std::cout << "test from Stete::LEVELING_UP";
         }
         window.display();
 
